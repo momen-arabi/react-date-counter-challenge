3fabcbb1f0a08dae9f6515518f8937502d353a18
momen-arabi <momenarabi25@gmail.com>
2025-01-08 19:24:26 +0200

    added-flowbite-and-test-component

--- src/App.jsx
+++ src/App.jsx
@@ -27,25 +27,25 @@ function App() {
   function handleStepDecrease() {
     if (steps > 1) {
       setSteps((prev) => prev - 1);
-      setCount(0);
+      // setCount(0);
       setDate(new Date());
     }
   }
 
   function handleStepIncrease() {
     setSteps((prev) => prev + 1);
-    setCount(0);
+    // setCount(0);
     setDate(new Date());
   }
 
   function handleCountDecrease() {
-    setCount((prev) => prev - 1);
+    setCount((prev) => prev - steps);
     let newDate = subtractDaysToDate(steps, date);
     setDate(newDate);
   }
 
   function handleCountIncrease() {
-    setCount((prev) => prev + 1);
+    setCount((prev) => prev + steps);
     let newDate = addDaysToDate(steps, date);
     setDate(newDate);
   }
@@ -54,11 +54,73 @@ function App() {
     <div className="h-screen flex items-center justify-center bg-slate-200">
       <div className="w-full text-center">
         <h1 className="text-4xl font-bold text-slate-900 uppercase">What day is it 🙄!</h1>
-        <div className="counter-container w-4/12 grid grid-cols-2 divide-x mx-auto my-6">
+
+        <form class="max-w-xs mx-auto">
+          <label for="bedrooms-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            Choose quantity:
+          </label>
+          <div class="relative flex items-center max-w-[11rem]">
+            <button
+              type="button"
+              id="decrement-button"
+              data-input-counter-decrement="bedrooms-input"
+              class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+            >
+              <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16" />
+              </svg>
+            </button>
+            <input
+              type="text"
+              id="bedrooms-input"
+              data-input-counter
+              data-input-counter-min="1"
+              data-input-counter-max="5"
+              aria-describedby="helper-text-explanation"
+              class="bg-gray-50 border-x-0 border-gray-300 h-11 font-medium text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full pb-6 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder=""
+              value="3"
+              required
+            />
+            <div class="absolute bottom-1 start-1/2 -translate-x-1/2 rtl:translate-x-1/2 flex items-center text-xs text-gray-400 space-x-1 rtl:space-x-reverse">
+              <svg class="w-2.5 h-2.5 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 8v10a1 1 0 0 0 1 1h4v-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v5h4a1 1 0 0 0 1-1V8M1 10l9-9 9 9"
+                />
+              </svg>
+              <span>Bedrooms</span>
+            </div>
+            <button
+              type="button"
+              id="increment-button"
+              data-input-counter-increment="bedrooms-input"
+              class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+            >
+              <svg
+                class="w-3 h-3 text-gray-900 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 18 18"
+              >
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+              </svg>
+            </button>
+          </div>
+          <p id="helper-text-explanation" class="mt-2 text-sm text-gray-500 dark:text-gray-400">
+            Please select the number of bedrooms.
+          </p>
+        </form>
+
+        {/* <div className="counter-container w-4/12 grid grid-cols-2 divide-x mx-auto my-6">
           <Counter label="Steps" steps={steps} decrease={handleStepDecrease} increase={handleStepIncrease} />
           <Counter label="Count" count={count} steps={steps} decrease={handleCountDecrease} increase={handleCountIncrease} />
         </div>
-        <Result count={count * steps} date={formatDate(date)} />
+        <Result count={count * steps} date={formatDate(date)} /> */}
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", // Include all files in the src directory
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
